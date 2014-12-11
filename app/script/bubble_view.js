40c1c8c31c9057c14d10ba77e68360f9cb7def71
ryaneof <ryaneof@gmail.com>
2014-12-11 19:10:19 +0800

    added simplified bubble explotion effect

--- app/script/bubble_view.js
+++ app/script/bubble_view.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var AppConfig = window.AppConfig;
 var bubbleTmpl = require('../hbs/bubble.hbs');
+var fragmentTmpl = require('../hbs/fragment.hbs');
 
 module.exports = Backbone.View.extend({
 
@@ -39,8 +40,10 @@ module.exports = Backbone.View.extend({
 
   render: function () {
 
-    this.bubble.posX += (this.bubble.diameter / 2);
-    this.bubble.posY += (this.bubble.diameter / 2);
+    _.extend(this.bubble, {
+      relativePosX: this.bubble.diameter / 2,
+      relativePosY: this.bubble.diameter / 2
+    });
 
     this.setElement(this.template(this.bubble));
     this.bindTouchEvent();
@@ -56,8 +59,89 @@ module.exports = Backbone.View.extend({
     });
   },
 
+  triggerExplotion: function () {
+
+    var self= this;
+
+    this.explotion = window.setTimeout(function () {
+      // todo: bubble clicked, disappear animation
+      var diameter = self.bubble.diameter;
+      self.$wrapper.css({
+        width: (diameter / 2) + 'px',
+        height: (diameter / 2) + 'px',
+        fontSize: '7px',
+        lineHeight: (diameter / 2) + 'px',
+        top: (diameter / 4) + 'px',
+        left: (diameter / 4) + 'px'
+      });
+
+      self.triggerFragmental();
+
+      window.clearTimeout(self.explotion);
+      window.clearTimeout(self.destruction);
+    }, 50);
+  },
+
+  triggerFragmental: function () {
+    var self = this;
+
+    this.fragmental = window.setTimeout(function () {
+      var htmlArr = [];
+      var fragments = [];
+      var htmlStr = '';
+      var color = self.bubble.color;
+      var diameter = self.bubble.diameter;
+      
+      for (var i = 0; i < 10; i += 1) {
+        var d = _.random(2, diameter / 3);
+        fragments.push({
+          diameter: d,
+          color: self.bubble.color,
+          posX: (diameter - d) / 2,
+          posY: (diameter - d) / 2
+        });
+      }
+
+      self.$el.html(fragmentTmpl({
+        fragments: fragments
+      }));
+
+      window.clearTimeout(self.fragmental);
+
+      self.triggerSpreading();
+
+    }, 500);
+  },
+
+  triggerSpreading: function () {
+
+    // todo: reduce fragment bubble count, 
+
+    var self = this;
+
+    self.spreading = window.setTimeout(function () {
+      var elFragments = self.$el.find('.bubble-fragment img');
+
+      for (var i = 0; i < 10; i += 1) {
+        elFragments[i].style.top = _.random(0, self.bubble.diameter / 2) + 'px';
+        elFragments[i].style.left = _.random(0, self.bubble.diameter / 2) + 'px';
+      }
+
+      self.$el.css({
+        top: (self.bubble.posY - 50) + 'px'
+      });
+
+      self.removal = window.setTimeout(function () {
+        self.$el.remove();
+      }, 1500);
+      window.clearTimeout(self.spreading);
+
+    }, 50);
+
+  },
+
   onClickedBubble: function (e) {
-    var el = e.target;
+    var el = e.currentTarget;
 
     if (!!el.dataset.clicked) { return true; }
 
@@ -65,29 +149,10 @@ module.exports = Backbone.View.extend({
     var res = this.getResult(el.dataset);
     var score = parseInt(res.score);
 
-    this.explotion = window.setTimeout(function () {
-      // todo: bubble clicked, disappear animation
-      var diameter = self.bubble.diameter;
-      self.$el.css({
-        width: (diameter / 2) + 'px',
-        height: (diameter / 2) + 'px',
-        fontSize: '7px',
-        lineHeight: (diameter / 2) + 'px',
-        top: (self.bubble.posY - diameter / 4) + 'px',
-        left: (self.bubble.posX - diameter / 4) + 'px'
-      });
-
-      this.removal = window.setTimeout(function () {
-        self.$el.remove();
-      }, 500);
-
-      window.clearTimeout(self.explotion);
-      window.clearTimeout(self.destruction);
-    }, 500);
-
     el.dataset.clicked = '1';
-    el.innerHTML = (score > 0) ? '+' + score : score;
+    $(el).find('span').html((score > 0) ? '+' + score : score);
 
+    this.triggerExplotion();
     this.updateScore(res.score);
     this.updateTime(res.time);
   },
@@ -96,16 +161,19 @@ module.exports = Backbone.View.extend({
     // todo: 
     // 1. rewrite with animation frame
     // 2. reduce destuction timeout according to timer
+    // 3. remove image border.
 
     var self = this;
 
+    this.$wrapper = this.$el.find('.bubble-wrapper');
+
     this.creation = window.setTimeout(function () {
       var diameter = self.bubble.diameter;
-      self.$el.css({
+      self.$wrapper.css({
         width: diameter + 'px',
         height: diameter + 'px',
-        top: (self.bubble.posY - diameter / 2) + 'px',
-        left: (self.bubble.posX - diameter / 2) + 'px',
+        top: '0px',
+        left: '0px',
         fontSize: '14px',
         lineHeight: diameter + 'px'
         // transform: "scale(1)"
@@ -114,6 +182,7 @@ module.exports = Backbone.View.extend({
     }, 500);
 
     this.destruction = window.setTimeout(function () {
+      // todo: disable click event while destruction
       self.$el.css({
         opacity: 0
       });
